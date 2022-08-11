4df7256dcf01ecf6dd60b94d47c72a93ccad09e3
Kitti-sec <kitti.nagy.hu@gmail.com>
2022-08-11 20:39:47 +0200

    hirtv save articles #TODO bug around page 5

--- hirtv-selenium.py
+++ hirtv-selenium.py
@@ -1,6 +1,13 @@
 from selenium import webdriver
 import pandas as pd
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import StaleElementReferenceException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 
 
 getLink = input("Type in the Hir TV link without the current_page count number: ")
@@ -13,27 +20,39 @@ driver.implicitly_wait(3)
 driver.find_element(By.CLASS_NAME, "css-1k8602u").click()
 driver.implicitly_wait(3)
 
+
 #  20cikk van egy oldalon
 
 saveHeadlines = []
 saveArticleText = []
 saveLeadArticle = []
+saveArticleDate = []
 webPageName = 'Hir TV'
 pageIndex = 1
+
 while pageIndex <= numberOfPages:
     currentPage = getLink + str(pageIndex)
     driver.get(currentPage)
-    for i in range(0,20,1):
-        hirTvArticles = driver.find_elements(By.CLASS_NAME, 'focuspoint')
-        hirTvArticles[i].click()
-        saveHeadlines.append(driver.find_element(By.CSS_SELECTOR, "h1").text)
-        saveArticleText.append(driver.find_element(By.CLASS_NAME, 'article-content').text)
-        saveLeadArticle.append(driver.find_element(By.CLASS_NAME, 'font-weight-bold.article-lead').text)
-        driver.back()
-        driver.implicitly_wait(3)
-    pageIndex+= 1
+    hirTvArticles = driver.find_elements(By.CLASS_NAME, 'focuspoint')
+    for i in range(len(hirTvArticles)):
+        try:
+            hirTvArticles = driver.find_elements(By.CLASS_NAME, 'focuspoint')
+            driver.implicitly_wait(16)
+            hirTvArticles[i].click()
+            driver.implicitly_wait(3)
+            saveHeadlines.append(driver.find_element(By.CSS_SELECTOR, "h1").text)
+            saveArticleText.append(driver.find_element(By.CLASS_NAME, 'article-content').text)
+            saveLeadArticle.append(driver.find_element(By.CLASS_NAME, 'font-weight-bold.article-lead').text)
+            saveArticleDate.append(driver.find_element(By.CLASS_NAME, 'small.article-date').text)
+            driver.back()
+            driver.implicitly_wait(3)
+            pageIndex+= 1
+        except IndexError:
+            print('out of range inside loop')
+            continue
 
-dict = {'Title':saveHeadlines, 'Lead text': saveLeadArticle, 'Text':saveArticleText, 'News site': webPageName, 'Keyword': getKeyword}
+
+dict = {'Title':saveHeadlines, 'Lead text': saveLeadArticle, 'Text':saveArticleText, 'Date':saveArticleDate, 'News site': webPageName, 'Keyword': getKeyword}
 df= pd.DataFrame(dict)
 df.to_csv('articlesfromhirtv.csv', index=False, encoding='utf-8-sig')
 driver.quit()
