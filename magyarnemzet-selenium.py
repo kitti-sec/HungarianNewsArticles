862be0f7864541f2a4c160f5de2ce4f2a149b842
KITTI NAGY <kitti.nagy.hu@gmail.com>
2022-09-23 14:18:29 +0200

    Adblock browser extension when starting selenium

--- magyarnemzet-selenium.py
+++ magyarnemzet-selenium.py
@@ -1,4 +1,5 @@
 from email.header import Header
+import time
 from selenium import webdriver
 import pandas as pd
 from selenium.webdriver.common.by import By
@@ -9,17 +10,25 @@ import os.path
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import TimeoutException
 
+from selenium.webdriver.chrome.options import Options
+
+path_to_extension = r'C:\All\projects\future\Charles Study\MA thesis\app\propagandaanalysis\1.44.4_3'
+chrome_options = Options()
+chrome_options.add_argument('load-extension=' + path_to_extension)
+
+
+
 getLink = input("Type in the Magyar Nemzet link without the current_page count number: ")
 pageIndex = int(input('From (page) '))
 numberOfPages = int(input("To (page) "))
 getKeyword = input("What keyword did you search for? ")
 
-driver = webdriver.Chrome(executable_path='C:/Users/shiro/Downloads/chromedriver_win32/chromedriver')
-
+driver = webdriver.Chrome(executable_path='C:/Users/shiro/Downloads/chromedriver_win32/chromedriver',chrome_options=chrome_options)
+driver.create_options()
 driver.get(getLink)
 
 webPageName = 'Magyar Nemzet'
-# pageIndex = 440
+
 
 driver.implicitly_wait(3)
 driver.find_element(By.CSS_SELECTOR, "body > div.fc-consent-root > div.fc-dialog-container > div.fc-dialog.fc-choice-dialog > div.fc-footer-buttons-container > div.fc-footer-buttons > button.fc-button.fc-cta-consent.fc-primary-button").click()
@@ -32,7 +41,10 @@ while pageIndex <= numberOfPages:
     saveLink = []
     currentPage = getLink + str(pageIndex)
     driver.get(currentPage)
-    articles = driver.find_elements(By.CSS_SELECTOR, "body > app-root > app-base > app-search > div > div.search-feature > div.result-list > app-article-card > article > div.article-right.ng-star-inserted > a")
+    # articles = driver.find_elements(By.CSS_SELECTOR, "body > app-root > app-base > app-search > div > div.search-feature > div.result-list > app-article-card > article > div.article-right.ng-star-inserted > a")
+    articles = driver.find_elements(By.CSS_SELECTOR, "body > app-root > app-base > app-search > div > div.search-feature > div.result-list > app-article-card > article > a")
+    articlesWithPics = driver.find_elements(By.CSS_SELECTOR, "body > app-root > app-base > app-search > div > div.search-feature > div.result-list > app-article-card > article > div.article-right.ng-star-inserted > a")
+    articles.extend(articlesWithPics)
     lengthofList = len(articles)
     for i in range(lengthofList):
         saveSearchPage = driver.current_url
@@ -42,7 +54,12 @@ while pageIndex <= numberOfPages:
                 EC.presence_of_element_located((By.CLASS_NAME, 'result-list'))
             )
             
-            articles = driver.find_elements(By.CSS_SELECTOR, "body > app-root > app-base > app-search > div > div.search-feature > div.result-list > app-article-card > article > div.article-right.ng-star-inserted > a")
+            # articles = driver.find_elements(By.CSS_SELECTOR, "body > app-root > app-base > app-search > div > div.search-feature > div.result-list > app-article-card > article > div.article-right.ng-star-inserted > a")
+            articles = driver.find_elements(By.CSS_SELECTOR, "body > app-root > app-base > app-search > div > div.search-feature > div.result-list > app-article-card > article > a")
+            articlesWithPics = driver.find_elements(By.CSS_SELECTOR, "body > app-root > app-base > app-search > div > div.search-feature > div.result-list > app-article-card > article > div.article-right.ng-star-inserted > a")
+            articles.extend(articlesWithPics)
+
+
             articles[i].click()
             driver.implicitly_wait(4)
                
@@ -52,7 +69,7 @@ while pageIndex <= numberOfPages:
 
             saveHeadlines.append(driver.find_element(By.CLASS_NAME, "title").text)
 
-            # /// Add together ydifferent sections to one article ///
+            # /// Add together different sections to one article ///
             articleTextList = driver.find_elements(By.TAG_NAME, 'app-article-text')
             articleTextCombined = ''
             for i in articleTextList:
